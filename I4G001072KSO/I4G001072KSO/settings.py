4422de07308d4318de2a2cb429177134f1f4d935
Onimisea <onimisea@gmail.com>
2022-06-20 19:33:17 +0100

    edited some files

--- I4G001072KSO/I4G001072KSO/settings.py
+++ I4G001072KSO/I4G001072KSO/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-'blog',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -52,20 +52,19 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'I4G001072KSO.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
+},
 ]
 
 WSGI_APPLICATION = 'I4G001072KSO.wsgi.application'
@@ -85,10 +84,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+AUTH_PASSWORD_VALIDATORS = [{
+    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+},
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
--- I4G001072KSO/blog/models.py
+++ I4G001072KSO/blog/models.py
@@ -6,17 +6,15 @@ User = get_user_model()
 # Create your models here.
 
 class Post(models.Model):
-Title = models.CharField(max_length=200)
-Text = models.Textfield()
-Author = models.ForeignKey(User, on_delete = models.CASCADE)
-Created_date = models.DateTimeField()
-Published_date = models.DateTimeField()
-
-
-def __str__(self):
-return self.Title
-
-def __str__(self): 
-return self.Text
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete = models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+    def __str__(self):
+        return self.Title
+        
+    def __str__(self): 
+        return self.Text
 
 
